1f57d4d95f70fb402e1fad8b588d832c7601114b
Raul Davila <raul@wearebolt.co>
2018-04-18 14:42:29 -0500

    Agregar 'Quiénes somos' en contacto

--- .deploy/mup.js
+++ .deploy/mup.js
@@ -26,7 +26,7 @@ module.exports = {
     env: {
       // TODO: Change to your app's url
       // If you are using ssl, it needs to start with https://
-      ROOT_URL: 'http://test.com',
+      ROOT_URL: 'http://valiara.mx',
       MONGO_URL: 'mongodb://localhost/meteor',
     },
 
--- client/imports/app/contacto/contacto.component.ts
+++ client/imports/app/contacto/contacto.component.ts
@@ -10,17 +10,50 @@ declare var $:any;
 	styles: [ style ]
 })
 export class ContactoComponent {
+	aboutOpen: boolean;
+	slideCounter: number;
 
-	openNav(){
+	constructor(){
+		this.aboutOpen = false;
+		this.slideCounter = 0;
+	}
+
+	openNav() {
 		$("div#city, div#logo").css({"display":"block"});
 		$("div#over").css({"width":"100% "});
 	}
 
-		openAviso(){
-			$("div#privacidad").css({"height":"100%","display":"block"});
-		}
+	openAviso() {
+		$("div#privacidad").css({"height":"100%","display":"block"});
+	}
 
-		closeAviso(){
+	closeAviso() {
 		$("div#privacidad").css({"height":"0%","display":"none"});
+	}
+
+	openCloseAbout(device) {
+		var width = (device == 'mobile') ? '100%' : '70%';
+
+		if (!this.aboutOpen){
+			$("#courtain").css({"width": width});
+			setTimeout(function(){ $('#courtain-content-wrapper').css('opacity', 1); }, 400);
 		}
+		else {
+			$('#courtain-content-wrapper').css('opacity', 0);
+			$("#courtain").css({"width": 0});
+		}
+		this.aboutOpen = !this.aboutOpen;
+	}
+
+	changeSlide(right) {
+		var slides = document.getElementsByClassName("slide"); // Slides
+
+		if (right) { // Derecha: Si estamos en el último slide, reiniciamos, else le sumamos uno
+			this.slideCounter = ((this.slideCounter == slides.length - 1) ? 0 : this.slideCounter + 1);
+		} else { // Izquierda: Si estamos en el primer slide, nos vamos al último, else le restamos uno
+			this.slideCounter = ((!this.slideCounter) ? slides.length - 1 : this.slideCounter - 1);
+		}
+		$('.slide').css('opacity', '0');
+		$(slides[this.slideCounter]).css('opacity', '1');
+	}
 }
